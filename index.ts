eb0588e341c7315b6b4d2aeeec657746c921ae23
m2lovric <m2lovric@gmail.com>
2024-07-17 23:08:17 +0200

    Restructure issues route

--- index.ts
+++ index.ts
@@ -1,39 +1,13 @@
-import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import cors from 'cors';
+import { issueRouter } from './routes/issues';
 
-const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/issues', async (req, res) => {
-  const issues = await prisma.issue.findMany();
-  return res.json(issues);
-});
-
-app.get('/issues/:id', async (req, res) => {
-  const { id } = req.params;
-  const issue = await prisma.issue.findUnique({
-    where: { id: Number(id) },
-  });
-
-  res.json(issue);
-});
-
-app.post('/issues', async (req, res) => {
-  const { title, status } = req.body;
-
-  const result = await prisma.issue.create({
-    data: {
-      title,
-      status,
-    },
-  });
-
-  res.json(result);
-});
+app.use('/issues', issueRouter);
 
 const PORT = 4001;
 app.listen(PORT, () => {
